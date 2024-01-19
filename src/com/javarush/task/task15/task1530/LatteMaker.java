518570d6bc384e2fdfdfa49f30ff60a805544dfd
Zablotskyi <zablotskyivv@gmail.com>
2024-01-19 22:48:01 +0200

    additional classes created

---
+++ src/com/javarush/task/task15/task1530/LatteMaker.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task15.task1530;
+
+public class LatteMaker extends DrinkMaker {
+    @Override
+    void getRightCup() {
+
+    }
+
+    @Override
+    void putIngredient() {
+
+    }
+
+    @Override
+    void pour() {
+
+    }
+}
