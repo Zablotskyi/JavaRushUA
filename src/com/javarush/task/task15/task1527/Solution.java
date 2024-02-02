5e1a837b57806de427e1323ef5f981284755ad97
Zablotskyi <zablotskyivv@gmail.com>
2024-02-02 13:07:16 +0200

    completed the task1527: Парсер реквестов

--- src/com/javarush/task/task15/task1527/Solution.java
+++ src/com/javarush/task/task15/task1527/Solution.java
@@ -13,7 +13,29 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String url = reader.readLine();
         //напишите тут ваш код
+        String delimiter = "\\?";
+        String[] parameters = url.split(delimiter);
+        String[] parameter = parameters[1].split("&");
+        for (int i = 0; i < parameter.length; i++) {
+            int equalsIndex = parameter[i].indexOf("=");
+            if (parameter[i].contains("=")) {
+                System.out.print(parameter[i].substring(0, equalsIndex) + " ");
+                continue;
+            }
+            System.out.print(parameter[i] + " ");
+        }
 
+        System.out.println();
+        for (int i = 0; i < parameter.length; i++) {
+            if (parameter[i].contains("obj")) {
+                String[] tmp = parameter[i].split("=");
+                try {
+                    alert(Double.parseDouble(tmp[1]));
+                } catch (NumberFormatException e) {
+                    alert(tmp[1]);
+                }
+            }
+        }
     }
 
     public static void alert(double value) {
