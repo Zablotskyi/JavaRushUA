c773e544e848b814db4a758b157dd3b0ee276d27
Zablotskyi <zablotskyivv@gmail.com>
2024-02-20 16:58:42 +0200

    completed the task1617 Отсчет на гонках

--- src/com/javarush/task/task16/task1617/Solution.java
+++ src/com/javarush/task/task16/task1617/Solution.java
@@ -5,11 +5,13 @@ package com.javarush.task.task16.task1617;
 */
 
 public class Solution {
-    public static volatile int numSeconds = 3;
+    public static volatile int numSeconds = 4;
 
     public static void main(String[] args) throws InterruptedException {
         RacingClock clock = new RacingClock();
         //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
     }
 
     public static class RacingClock extends Thread {
@@ -19,6 +21,17 @@ public class Solution {
 
         public void run() {
             //add your code here - добавь код тут
+            try {
+                while (numSeconds > 0) {
+                    System.out.print(numSeconds + " ");
+                    Thread.sleep(1000);
+                    numSeconds--;
+                }
+                System.out.println("Марш!");
+            } catch (InterruptedException e) {
+                System.out.println("Прервано!");
+                throw new RuntimeException(e);
+            }
         }
     }
 }
