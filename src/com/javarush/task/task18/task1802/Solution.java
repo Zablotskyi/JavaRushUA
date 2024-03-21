75af7d80c575f3b9f0957fcb30ffdf74ee04dcab
Zablotskyi <zablotskyivv@gmail.com>
2024-03-21 19:10:56 +0200

    reworked task1802, namely, read the file not through the readAllByte() method (since this method is not in the JavaRush validator). I read the file using a while loop.

--- src/com/javarush/task/task18/task1802/Solution.java
+++ src/com/javarush/task/task18/task1802/Solution.java
@@ -1,8 +1,7 @@
 package com.javarush.task.task18.task1802;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -15,17 +14,13 @@ public class Solution {
         String fileName = scanner.nextLine();
 
         FileInputStream fis = new FileInputStream(fileName);
-        char[] array = new char[256];
-        byte oneByte;
-
+        byte[] array = new byte[1024];
         while (fis.available() > 0) {
-            oneByte = (byte) fis.read();
-            array[oneByte] = 'x';
+            fis.read(array);
         }
-        for (Character item : array) {
-            if (item != null)
-                System.out.println(item);
-            break;
-        }
+        Arrays.sort(array);
+        System.out.println(array[0]);
+        scanner.close();
+        fis.close();
     }
 }
