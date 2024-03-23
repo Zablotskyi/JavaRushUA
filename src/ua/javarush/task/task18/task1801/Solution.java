da9b176450d8de396eee6ec426a49b7ec8c8e75a
Zablotskyi <zablotskyivv@gmail.com>
2024-03-23 20:41:31 +0200

    started to perform the task1801

---
+++ src/ua/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Максимальний байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
